c64d8c67fb08d59dd63358ab983d5c6cd769180b
yangyh <136585913@qq.com>
2017-04-28 20:12:19 +0800

    frist commit

---
+++ controller/controller.js
@@ -0,0 +1,38 @@
+exports.controller = function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json;charset=utf-8');
+    var params = req.params
+
+    // 目录 
+    var directories = params.service
+    var service = require("../service/" + directories + ".js")
+    service.DataApi(params, function (data) {
+        res.send(data)
+    })
+}
+
+
+
+
+
+
+exports.test = function (req, res, next) {
+    res.send('GET success');
+}
+
+exports.test_post = function (req, res, next) {
+
+    // post、get请求都能用req.params接收参数
+    var password = req.params.password
+    var userInfo = {
+        username: 'yangyh',
+        password: 666666
+    }
+    var result = {}
+    if (password == userInfo.password) {
+        result.Issuccess = 0
+    } else {
+        result.Issuccess = 1
+    }
+    res.send(result);
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+var restify = require('restify');
+var controller = require('./controller/controller')
+
+var server = restify.createServer();
+var server_port = 5500;
+
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser({
+	mapParams: true
+}));
+server.use(restify.dateParser());
+server.use(restify.jsonp());
+server.use(restify.gzipResponse());
+// 处理跨域
+server.use(restify.CORS()); 
+
+// 路由配置 
+server.get('/api/:service/:api', controller.controller);
+server.post('/api/:service/:api', controller.controller);
+
+// test
+server.get('/test/test', controller.test);
+server.post('/test/test_post', controller.test_post);
+
+
+
+server.listen(server_port, function () {
+    console.log('server start at ' + server_port);
+});
